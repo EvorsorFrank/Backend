8dfa74263f2a87bcc4e17f662f4438093558bec2
EvorsorFrank <161805263+SRDSurveying@users.noreply.github.com>
2024-03-22 16:42:35 +0800

    added

--- app/api.py
+++ app/api.py
@@ -7,14 +7,6 @@ import tensorflow as tf
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 @app.get('/')
 def hello_world():
     return 'Hello World'
